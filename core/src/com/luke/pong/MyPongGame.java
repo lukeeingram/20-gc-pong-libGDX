bd995cc0dd781704d7df9636d6426dfc47245b37
Luke Ingram <lingram217@gmail.com>
2024-05-12 15:39:33 -0400

    Adds useful methods, basic enemy movement, collision
    WIP commit.

--- core/src/com/luke/pong/MyPongGame.java
+++ core/src/com/luke/pong/MyPongGame.java
@@ -18,8 +18,9 @@ public class MyPongGame extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-		player = new Player(20, 20, 25.0f, 100.0f, 200);
+		player = new Player(20, 20, 25.0f, 100.0f, 200.0f);
 		ball = new Ball(ballStartPos.x, ballStartPos.y, 20.0f, 20.0f, 100.0f);
+		enemy = new EnemyAI(GameConstants.SCREEN_WIDTH - 40, GameConstants.SCREEN_HEIGHT / 2, 25.0f, 100.0f, 200.0f);
 
 		// Get keyboard input for specific presses.
 		Gdx.input.setInputProcessor(new InputAdapter() {
@@ -41,26 +42,48 @@ public class MyPongGame extends ApplicationAdapter {
 		// Render objects to the screen
 		player.draw();
 		ball.draw();
+		enemy.draw();
 
+		// Move the enemy paddle
+		handleEnemyMovement();
+
+		// Handle collisions with the paddles
 		checkPlayerCollision();
 		checkEnemyCollision();
 	}
 
 	public void checkPlayerCollision() {
 		if (ball.getBallX() <= player.getPlayerX() + player.getPlayerWidth() &&
+			ball.getBallY() >= player.getPlayerY() &&
 			ball.getBallY() <= player.getPlayerY() + player.getPlayerHeight()) {
 			ball.flipXDirection();
 		}
 	}
 
-	public void checkEnemyCollision() {
+	public void handleEnemyMovement() {
+		if (ball.getBallX() >= GameConstants.SCREEN_WIDTH / 2 && ball.getBallY() > enemy.getEnemyY()) {
+			enemy.moveUp();
+		}
 
+		if (ball.getBallX() >= GameConstants.SCREEN_WIDTH / 2 && ball.getBallY() < enemy.getEnemyY()) {
+			enemy.moveDown();
+		}
+	}
+
+	public void checkEnemyCollision() {
+		if (ball.getBallX() + ball.getBallWidth() >= enemy.getEnemyX() &&
+				ball.getBallY() >= enemy.getEnemyY() &&
+				ball.getBallY() <= enemy.getEnemyY() + enemy.getEnemyHeight()) {
+			ball.flipXDirection();
+		}
 	}
 
 	@Override
 	public void dispose () {
 		player.dispose();
 		ball.dispose();
+		enemy.dispose();
+
 		Gdx.app.exit();
 		System.exit(0);
 	}
